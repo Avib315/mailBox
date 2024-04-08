f2f5ea0287c7732afd1142b6421d27201b6cfdd7
אבי בראשי <avibarashi315248@gmail.com>
2024-04-08 11:46:48 +0300

    update server no message

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express"),
     app = express(),
     CORS = require("cors"),
-    PORT = 3001
-require("dotenv").config()
-app.use(CORS());
-app.use(express.json());
-app.use("/email", require("./routes/chat.router.js"));
-app.use("/user", require("./routes/user.router.js"));
-require("./DL/db.js").connect()
+    PORT = 3001,
+    test = require("./DL/test_data");
+    test.go();
+// require("dotenv").config()
+// app.use(CORS());
+// app.use(express.json());
+// app.use("/email", require("./routes/chat.router.js"));
+// app.use("/user", require("./routes/user.router.js"));
+// require("./DL/db.js").connect()
 app.listen(PORT, () => {
     console.log(`\x1b[42m [index.js] server is runing in port ${PORT} \x1b[0m`);
 })
